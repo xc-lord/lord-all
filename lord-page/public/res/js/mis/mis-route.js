e7a1004ea37c579921f295dae94151a9c8dfc00c
xiaocheng <miraclelord@163.com>
2017-07-04 18:30:32 +0800

    移动文件目录

--- lord-page/public/res/js/mis/mis-route.js
+++ lord-page/public/res/js/mis/mis-route.js
@@ -6,91 +6,91 @@
 var routes = {
     //后台用户
     '/misUser': function () {
-        $("#showView").load("/mis/misUser/misUserList.html");
+        $("#showView").load("/mis/mis/misUser/misUserList.html");
     },
     '/misUser/add': function () {
-        $("#showView").load("/mis/misUser/misUserAdd.html");
+        $("#showView").load("/mis/mis/misUser/misUserAdd.html");
     },
     '/misUser/edit/:id': function (id) {
         pageParam = Object.assign({}, {id: id});//设置页面参数
-        $("#showView").load("/mis/misUser/misUserEdit.html");
+        $("#showView").load("/mis/mis/misUser/misUserEdit.html");
     },
     //后台用户角色
     '/misRole': function () {
-        $("#showView").load("/mis/misRole/misRoleList.html");
+        $("#showView").load("/mis/mis/misRole/misRoleList.html");
     },
     '/misRole/add': function () {
-        $("#showView").load("/mis/misRole/misRoleAdd.html");
+        $("#showView").load("/mis/mis/misRole/misRoleAdd.html");
     },
     '/misRole/edit/:id': function (id) {
         pageParam = Object.assign({}, {id: id});//设置页面参数
-        $("#showView").load("/mis/misRole/misRoleEdit.html");
+        $("#showView").load("/mis/mis/misRole/misRoleEdit.html");
     },
     //系统菜单
     '/misMenu': function () {
-        $("#showView").load("/mis/misMenu/misMenuTree.html");
+        $("#showView").load("/mis/mis/misMenu/misMenuTree.html");
     },
     '/misMenu/add': function () {
-        $("#showView").load("/mis/misMenu/misMenuAdd.html");
+        $("#showView").load("/mis/mis/misMenu/misMenuAdd.html");
     },
     '/misMenu/edit/:id': function (id) {
         pageParam = Object.assign({}, {id: id});//设置页面参数
-        $("#showView").load("/mis/misMenu/misMenuEdit.html");
+        $("#showView").load("/mis/mis/misMenu/misMenuEdit.html");
     },
     //文件管理
     '/sysFile': function () {
-        $("#showView").load("/mis/sysFile/sysFileList.html");
+        $("#showView").load("/mis/sys/sysFile/sysFileList.html");
     },
     '/sysFile/add': function () {
-        $("#showView").load("/mis/sysFile/sysFileAdd.html");
+        $("#showView").load("/mis/sys/sysFile/sysFileAdd.html");
     },
     '/sysFile/edit/:id': function (id) {
         pageParam = Object.assign({}, {id: id});//设置页面参数
-        $("#showView").load("/mis/sysFile/sysFileEdit.html");
+        $("#showView").load("/mis/sys/sysFile/sysFileEdit.html");
     },
     //站点管理
     '/sysSite': function () {
-        $("#showView").load("/mis/sysSite/sysSiteList.html");
+        $("#showView").load("/mis/sys/sysSite/sysSiteList.html");
     },
     '/sysSite/add': function () {
-        $("#showView").load("/mis/sysSite/sysSiteAdd.html");
+        $("#showView").load("/mis/sys/sysSite/sysSiteAdd.html");
     },
     '/sysSite/edit/:id': function (id) {
         pageParam = Object.assign({}, {id: id});//设置页面参数
-        $("#showView").load("/mis/sysSite/sysSiteEdit.html");
+        $("#showView").load("/mis/sys/sysSite/sysSiteEdit.html");
     },
     //文章标签
     '/cmsArticle': function () {
-        $("#showView").load("/mis/cmsArticle/cmsArticleList.html");
+        $("#showView").load("/mis/cms/cmsArticle/cmsArticleList.html");
     },
     '/cmsArticle/add': function () {
-        $("#showView").load("/mis/cmsArticle/cmsArticleAdd.html");
+        $("#showView").load("/mis/cms/cmsArticle/cmsArticleAdd.html");
     },
     '/cmsArticle/edit/:id': function (id) {
         pageParam = Object.assign({}, {id: id});//设置页面参数
-        $("#showView").load("/mis/cmsArticle/cmsArticleEdit.html");
+        $("#showView").load("/mis/cms/cmsArticle/cmsArticleEdit.html");
     },
     //文章标签
     '/cmsTags': function () {
-        $("#showView").load("/mis/cmsTags/cmsTagsList.html");
+        $("#showView").load("/mis/cms/cmsTags/cmsTagsList.html");
     },
     '/cmsTags/add': function () {
-        $("#showView").load("/mis/cmsTags/cmsTagsAdd.html");
+        $("#showView").load("/mis/cms/cmsTags/cmsTagsAdd.html");
     },
     '/cmsTags/edit/:id': function (id) {
         pageParam = Object.assign({}, {id: id});//设置页面参数
-        $("#showView").load("/mis/cmsTags/cmsTagsEdit.html");
+        $("#showView").load("/mis/cms/cmsTags/cmsTagsEdit.html");
     },
     //文章分类
     '/cmsCategory': function () {
-        $("#showView").load("/mis/cmsCategory/cmsCategoryTree.html");
+        $("#showView").load("/mis/cms/cmsCategory/cmsCategoryTree.html");
     },
     '/cmsCategory/add': function () {
-        $("#showView").load("/mis/cmsCategory/cmsCategoryAdd.html");
+        $("#showView").load("/mis/cms/cmsCategory/cmsCategoryAdd.html");
     },
     '/cmsCategory/edit/:id': function (id) {
         pageParam = Object.assign({}, {id: id});//设置页面参数
-        $("#showView").load("/mis/cmsCategory/cmsCategoryEdit.html");
+        $("#showView").load("/mis/cms/cmsCategory/cmsCategoryEdit.html");
     },
 };
 var router = Router(routes);
