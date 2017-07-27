077c220391e9293820f7c91d99c2669dc157d676
xiaocheng <miraclelord@163.com>
2017-07-27 16:54:22 +0800

    lord-page启动的shell脚本

--- lord-db/shell/page.sh
+++ lord-db/shell/page.sh
@@ -2,14 +2,31 @@
 ps -fe|grep node |grep -v grep
 if [ $? -ne 0 ]
 then
-echo "start node process....."
-cd /data/app/server/node/lord-page/
+echo "start node lord-page process....."
+echo "Update from github.com!"
+cd /data/app/src/lord-all
+git pull origin master
+echo "copy file to server path!"
+rm -fr /data/app/server/node/lord-page/
+cp -r /data/app/src/lord-all/lord-page/ /data/app/server/node
+echo "node lord-page install project!"
+cd /data/app/server/node/lord-page
+cnpm install
 nohup node bin/www >/data/app/server/node/log-lord-page.log 2>&1 &
-echo "lord-page nodejs server starting !"
+echo "node lord-page server starting!"
 else
-echo "node is runing....."
+echo "node lord-page is runing....."
+echo "Update from github.com!"
+cd /data/app/src/lord-all
+git pull origin master
+echo "shutdown node lord-page!"
 ps -ef | grep node | grep -v grep | awk '{print $2}' | xargs kill -9
-cd /data/app/server/node/lord-page/
+echo "copy file to server path!"
+rm -fr /data/app/server/node/lord-page/
+cp -r /data/app/src/lord-all/lord-page/ /data/app/server/node
+echo "node install project!"
+cd /data/app/server/node/lord-page
+cnpm install
 nohup node bin/www >/data/app/server/node/log-lord-page.log 2>&1 &
-echo "lord-page nodejs server restarting !"
+echo "node lord-page server restarting!"
 fi
