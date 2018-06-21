1654bdf36eb600345ae091ea53b0fbcfb9b9180f
xiaocheng <miraclelord@163.com>
2018-06-21 18:01:41 +0800

    首页实现

--- lord-page/routes/indexController.js
+++ lord-page/routes/indexController.js
@@ -9,22 +9,7 @@
 
     /* 首页 */
     router.get('/', function (req, res, next) {
-        console.log("req.host = " + req.host);
-        console.log("req.port = " + global.serverPort);
-        console.log("req.originalUrl = " + req.originalUrl);
-        console.log("req.url = " + req.url);
-        console.log("req.path = " + req.path);
-        var api = {
-            districtList: {
-                url: '/api/common/listDistrict.do'
-            }
-        };
-        dataUtils.get(req, api, function (err, data) {
-            data.title = "我的首页";
-            data.keywords = "我的首页";
-            data.description = "我的首页";
-            res.render('./index/index', data);//渲染页面
-        });
+        res.redirect('/edu/');
     });
 
 
