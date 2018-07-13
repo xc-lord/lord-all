9b813b205468b72e64b760fbf63eb1f63bad2b08
xiaocheng <miraclelord@163.com>
2018-07-13 18:08:03 +0800

    检查服务是否可用

--- lord-page/routes/indexController.js
+++ lord-page/routes/indexController.js
@@ -4,6 +4,7 @@
     var path = require('path');
     var logger = require('../lib/logger')(path.basename(__filename));
     var dataUtils = require('../lib/dataUtils');
+    var asyncUtils = require('../lib/asyncUtils');
     var express = require('express');
     var router = express.Router();
 
@@ -12,6 +13,18 @@
         res.redirect('/edu/');
     });
 
+    /* 检查服务是否可用 */
+    router.get('/health.do', function (req, res, next) {
+        asyncUtils.get({
+                url: '/api/health.do',
+                req: req
+            },
+            "json",
+            function (err, response) {
+                console.info(response);
+                res.json(response);
+            });
+    });
 
     module.exports = router;
 })();
